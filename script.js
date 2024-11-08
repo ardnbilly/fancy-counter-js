89bf13bbca16dd72372f4c585e6d833e23914118
Billy <billyardhana12@gmail.com>
2024-11-08 22:20:39 +0700

    Select all interactive element for dom manipulation

---
+++ script.js
@@ -0,0 +1,5 @@
+const counterTitle = document.querySelector(".counter__title");
+const counterValue = document.querySelector(".counter__value");
+const counterResetBtn = document.querySelector(".counter__reset-btn");
+const counterIncreaseBtn = document.querySelector(".counter__button--increase");
+const counterDecreaseBtn = document.querySelector(".counter__button--decrease");
